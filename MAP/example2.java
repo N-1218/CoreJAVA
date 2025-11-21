93a1eb437b26efd27d00f550d3787974747680ec
Namrata Malusare <malusarenamrata88@gmail.com>
2025-11-21 15:39:47 +0530

    Add files via upload

---
+++ MAP/example2.java
@@ -0,0 +1,23 @@
+package MAP;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class example2 {
+    public static void main(String []args){
+       String name="NAMRATA";
+	 char[]Char=name.toCharArray();
+	 Map<Character,Integer> map=new HashMap<>();
+		for(int i=0;i<Char.length;i++) {
+			int Counter=1;
+			if(map.containsKey(Char[i])) {
+				Counter=map.get(Char[i])+1;
+				map.put(Char[i], Counter);
+			}
+			else {
+				map.put(Char[i], Counter);
+			}
+		}
+		System.out.println(map);
+    }
+}
