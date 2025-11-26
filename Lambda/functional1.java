18e31b3e250db5237ab8d6584aeea43ad0d89127
Namrata Malusare <malusarenamrata88@gmail.com>
2025-11-26 11:54:58 +0530

    Refactor functional1.java to use multiple lambda expressions

--- Lambda/functional1.java
+++ Lambda/functional1.java
@@ -2,22 +2,61 @@ package Lambda;
 
 public class functional1 {
      public static void main(String[] args) {
-        
-        Students<Integer> stud = (day) -> day - 10; 
-        System.out.println(stud.Present_day(50));
-    
+    	//1
+     Students<String>stud=(i,j)->{return i+" "+j; };
+     System.out.println("Student name : "+stud.Attendance("Namrata", "Malusare"));
+     
+     //2
+     Students<Integer> stud2 = (i, j) -> {return (i / j) * 100;};
+     int j = 50;
+     j = stud2.Attendance(46, j);
+     System.out.println("Total Marks: " + j);
+     
+     
+     //3: Find bigger string
+     Students<String> stud3 = (a, b) -> { 
+         if (a.length() > b.length()) {
+             return a;
+         } else {
+             return b;
+         }
+     };
 
-    Students1<Integer>stud1=(day1)->System.out.print(day1);
-    stud1.Present_day(20);
+     String bigger = stud3.Attendance("JavaScript", "Programming");
+     System.out.println("Bigger String is : " + bigger);
+
+
+     //4: Find smallest string
+     Students<String> stud4 = (y, z) -> {
+         if (y.length() < z.length()) {
+             return y;
+         } else {
+             return z;
+         }
+     };
+
+     String smallest = stud4.Attendance("Python", "Programing");
+     System.out.println("Smallest String is : " + smallest);
+
+     
+     //5
+     Students<String>stud5=(x,a)->{
+    	 if(x==a) {
+    		 return x;
+    	 }else {
+    		 return a;
+    	 }
+     };
+     String compare=stud5.Attendance("namrata", "namrata");
+     System.out.println("Both name are same : "+compare);
+     
     }
 }
 
 @FunctionalInterface
 interface Students<T>{
-    T Present_day(T t1);
+    T Attendance(T t1,T t2);
 }
 
-@FunctionalInterface
-interface Students1<T>{
-    void Present_day(T t1);
-}
+
+
