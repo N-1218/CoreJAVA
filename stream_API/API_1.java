5b0e336133aa8b03fceb5e9e79a1783a97ed3ff5
Namrata Malusare <malusarenamrata88@gmail.com>
2025-12-02 15:25:16 +0530

    Add files via upload

---
+++ stream_API/API_1.java
@@ -0,0 +1,15 @@
+package stream_API;
+
+public class API_1 {
+    
+public static void main(String[] args) {
+		List<Integer>numbers=new Vector<Integer>();
+		numbers.add(1);
+		numbers.add(2);
+		numbers.add(3);
+		numbers.add(4);
+		System.out.println("Numbers : "+numbers);
+		numbers.stream().filter((i)->{return i%2==0;}).forEach((i)->{System.out.println(i);});
+	}
+
+}
