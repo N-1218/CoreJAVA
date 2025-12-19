19818e291453b928be58d73617db6f3ac9c1bcce
Namrata Malusare <malusarenamrata88@gmail.com>
2025-12-19 07:38:58 +0530

    Add files via upload

---
+++ Collection/bifunction.java
@@ -0,0 +1,13 @@
+public class bifunction {
+    
+BiFunction<String,String,Integer>bi=demo::add;
+	System.out.println(bi.apply("abc", new String("abc")));
+
+	}
+
+}
+class demo{
+	public static int add(String a,String b) {
+		return a.length()+b.length();
+	}
+}
