7fc3416f1445c1b4f848a80f1ee701949db8b128
Namrata Malusare <malusarenamrata88@gmail.com>
2025-12-19 07:43:04 +0530

    Add files via upload

---
+++ Collection/sumofallelement.java
@@ -0,0 +1,15 @@
+public class sumofallelement {
+    
+public static void main(String[] args) {
+		
+	List<Integer>list=Arrays.asList(1,2,3,4,5);
+	int sum=1;
+	for (int n:list) {
+		sum*=n;
+		
+	}
+	System.out.println(sum);
+	}
+		
+
+	}
