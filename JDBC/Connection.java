9fa2d1e564ad126bd8395324f297cd489e68bdd3
N-1218 <malusarenamrata88@gmail.com>
2026-01-09 13:59:38 +0530

    JDBC Connection

--- JDBC/Connection.java
+++ JDBC/Connection.java
@@ -6,10 +6,13 @@ import java.sql.Connection;
 public class Connection {
     
 public static void main(String[] args) throws ClassNotFoundException, SQLException {
+    //Registring a Driver
 		Class<?> name =Class.forName("com.mysql.cj.jdbc.Driver");
+        //Establish(Opening)Connection
 		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/JDBC", "root", "1218");
-		
+		//create Statement(SQL Queries)
 		Statement stmt = con.createStatement();
+        //insert SQL Queries
 		int num=stmt.executeUpdate("insert into jdbc values(1,'Vinayk');");
 	
 	
