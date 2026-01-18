904e8adb4830b2e66532d995439b976df9692f9b
N-1218 <malusarenamrata88@gmail.com>
2026-01-18 08:40:42 +0530

    Sum of numbers

---
+++ Stream_API/Sumofnumber.java
@@ -0,0 +1,16 @@
+package stream_API;
+
+public class Sumofnumber {
+    
+public static void main(String[] args) {
+		List<Integer>li=Arrays.asList(1,2,3,4,5);
+
+		int sum=0;
+		for(int num:li) {
+			sum+=num;
+			
+		}
+		System.out.println(sum);
+	}
+
+}
